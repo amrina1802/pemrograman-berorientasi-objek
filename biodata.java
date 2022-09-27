6bb81966ccd5ceb0c3e0c12cb1fde52eec6213de
amrina1802 <102012971+amrina1802@users.noreply.github.com>
2022-09-27 14:03:32 +0700

    Add files via upload

---
+++ biodata.java
@@ -0,0 +1,21 @@
+public class biodata {
+    public static void main(String[] args) {
+
+        String namadpn = "amrina";
+        String namablk = " rosada";
+        String dsn = "aik dareq";
+        String jk = "perempuan";
+        int tgl=18;
+        int bln=02;
+        int thn1=2002;
+        int thn2=2022;
+        int thnow=thn2-thn1;
+
+
+        System.out.println ("Nama Lengkap : " + namadpn+namablk);
+        System.out.println  (" Tanggal Lahir :"+dsn+" "+tgl+"/"+bln+"/ "+thn1);
+        System.out.println ("Umur : "+thnow+" "+"Tahun" );
+        System.out.println(" Jenis Kelamin :" +jk);
+
+    }
+}
